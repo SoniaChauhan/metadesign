28db024089b1501498fe31e25c719310d27e4ffa
Sonia Chauhan <s.chauhan@sanchalansoft.com>
2020-11-28 04:39:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+declare var $: any;
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'metadesign';
+
+  constructor(public modalService: BsModalService) {}
+  modalRef:BsModalRef;
+  ngOnInit() {
+  }
+  public openModal(template:TemplateRef<any>){
+    this.modalRef = this.modalService.show(template);
+  }
+
+  detailsObj = {
+    dateOfBirth:moment().format('DD-MMM-YYYY'),
+    nikName:null,
+    fullName:null
+  }
+
+/////////////////////////   DATE FORMAT ///////////////////////////////////////////
+monthName = ["","Jan","Feb","March","April","May","June","July","Aug","Sep","Oct","Nov","Dec"];
+dateToConvert;
+issueDateChange = () => {
+ this.dateToConvert = $(".issueDatePick").val();
+ this.detailsObj.dateOfBirth = this.changeDateFormate(this.dateToConvert);
+ $(".issueDatePick").val(this.detailsObj.dateOfBirth);
+};
+
+changeDateFormate(val) {
+ let val2 = String(val).split("/");
+ let newDate = val2[1] + "-" + this.monthName[val2[0]] + "-" + val2[2];
+ return newDate;
+}
+
+detailsArr = [];
+
+add = () =>{
+ let val = String(this.dateToConvert).split("/");
+ let monthName = ["","Jan","Feb","March","April","May","June","July","Aug","Sep","Oct","Nov","Dec"];
+ this.detailsObj.dateOfBirth = monthName[val[0]] + " " + val[1] + "," +  val[2];
+
+ this.detailsArr.push(JSON.parse(JSON.stringify(this.detailsObj)))
+ this.detailsObj.fullName = null;
+ this.detailsObj.nikName = null;
+ this.modalService.hide();
+}
+
+delete = (index) => {
+this.detailsArr.splice(index,1);
+}
+
+
+markAsDone = (_index) => {
+  $(`.isShow${_index}`).show();
+}
+
+public closeModal() {
+   this.modalService.hide();
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
+import {ModalModule} from 'ngx-bootstrap/modal';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { FormsModule } from '@angular/forms';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    OwlDateTimeModule,
+    MatMenuModule,
+    OwlNativeDateTimeModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
